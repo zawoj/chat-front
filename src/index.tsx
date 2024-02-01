77f45361307be3860af1b530cf9fe1f6fc395d0a
Zawoj <zawojskikacperkontakt@gmail.com>
2024-02-01 13:14:41 +0100

    feat: login and register forms

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode='dark' />
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode='dark' />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
